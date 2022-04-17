ddd5ba48207e641ee4616f3d873a32fcfc9fbee9
KOTARO SUTO <ng1a5ss43@gmail.com>
2022-04-17 16:33:44 -0700

    Clean up

--- problems/LongestPalindromicSubstring/main.swift
+++ problems/LongestPalindromicSubstring/main.swift
@@ -9,31 +9,25 @@ class Solution {
         for i in 0 ..< s.count {
             var l = i
             var r = i
-            
             while 0 <= l && r < s.count && s[l] == s[r] {
                 if r - l + 1 > resLen {
                     res = String(s[l ... r])
                     resLen = r - l + 1
                 }
-                
                 l -= 1
                 r += 1
             }
-            
+
             l = i
             r = i+1
-
             while 0 <= l && r < s.count && s[l] == s[r] {
                 if r - l + 1 > resLen {
                     res = String(s[l ... r])
                     resLen = r - l + 1
                 }
-                
                 l -= 1
                 r += 1
             }
-            
-            
         }
         return res
     }
